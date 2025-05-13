8ee7b9e25e6074c0180db27720c7c70f4a53f552
i-budonnyi <polandgdansk2020@gmail.com>
2025-05-13 14:30:55 +0300

    nazva

---
+++ routes/notifications.js
@@ -0,0 +1,10 @@
+﻿const express = require("express");
+const router = express.Router();
+const notificationController = require("../controllers/notificationController");
+
+router.post("/", notificationController.addNotification);
+router.get("/:userId", notificationController.getUserNotifications);
+router.patch("/:id/read", notificationController.markAsRead);
+router.delete("/:userId", notificationController.deleteAllNotifications);
+
+module.exports = router;
