16289ec785a426e9399f1fd68ea46b916c1442ad
i-budonnyi <polandgdansk2020@gmail.com>
2025-05-28 12:36:13 +0300

    hjgfhg544

---
+++ socket.js
@@ -0,0 +1,41 @@
+﻿let io;
+
+function initIO(server) {
+  io = require("socket.io")(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("🟢 Socket connected:", socket.id);
+
+    socket.on("register", (userId) => {
+      console.log(`🔔 Користувач зареєструвався: ${userId}`);
+      socket.join(`notification_${userId}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("🔴 Socket disconnected:", socket.id);
+    });
+  });
+
+  return io;
+}
+
+function getIO() {
+  if (!io) {
+    throw new Error("Socket.io не ініціалізовано");
+  }
+  return io;
+}
+
+function sendNotification(userId, message) {
+  getIO().to(`notification_${userId}`).emit("notification", {
+    message,
+    is_read: false,
+  });
+}
+
+module.exports = { initIO, getIO, sendNotification };
