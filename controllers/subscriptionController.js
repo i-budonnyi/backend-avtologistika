d5ba7613fcdd0794c9c008f50c2a9653e8e85782
i-budonnyi <polandgdansk2020@gmail.com>
2025-06-24 13:45:38 +0300

    ())(UOI

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -187,7 +187,7 @@ const unsubscribeFromEntry = async (req, res) => {
     res.status(200).json({ message: "Підписка видалена." });
   } catch (err) {
     console.error("❌ Помилка при відписці:", err.message);
-    res.status(500).json({ error: "Не вдалося відписатися", details: err.message });
+    res.status(500).json({ error: " вдалося відписатися", details: err.message });
   }
 };
 
